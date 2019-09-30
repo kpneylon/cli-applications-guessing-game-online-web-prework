3d92742412ab1d56028e0bcab0f45880e34521d0
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-09-30 13:40:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,21 @@
-# Code your solution here!
+
+def run_guessing_game
+random_number = rand(1..6)
+ loop do
+   print "Guess the number between 1 and 6"
+   answer = gets.chomp
+   if answer == "e"
+     puts "The number was #{random_number}."
+     break
+   else
+     if answer == random_number
+       puts "You guessed the correct number!"
+       break
+     else
+       puts "Sorry! The computer guessed #{random_number}"
+     end
+     if user_input == "exit"
+     puts "Goodbye!"
+   end
+ end
+end 
