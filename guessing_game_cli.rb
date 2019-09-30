de735ff659731ba26afa08ef75c8379c75deb102
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-09-30 14:05:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
     if user_input.to_i == comp_num
       puts "You guessed the correct number!"
     else
-      puts "The computer guessed #{comp_num}."
+      puts "Sorry! The computer guessed #{comp_num}."
     end
     comp_num = rand(1..6)
     user_input = gets.chomp
